a79e9b3c5b3f2c6d0afde7010ae630a3eb29c668
Filip Georgiev <philip.georgiev@productmadness.com>
2021-05-18 11:47:36 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RandomNumberGeneratorB
+
+A description of this package.
---
+++ Sources/RandomNumberGeneratorB/RandomNumberGeneratorB.swift
@@ -0,0 +1,7 @@
+struct RandomNumberGeneratorB {
+    var text = "Hello, World!"
+    
+    public func HelloToYou()-> String {
+        return "Hellow to you !"
+    }
+}
